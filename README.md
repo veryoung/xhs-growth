4a31279ef7a61002fefeb46dcaec914540d536a0
mofeng <wanyang@xiaohongshu.com>
2025-01-07 16:23:38 +0800

    feat: 初始化应用

---
+++ README.md
@@ -0,0 +1,9 @@
+# XHS_GROWTH
+
+小红书增长团队对外输出裂变相关核心能力
+
+## Installation
+
+```bash
+pnpm add my-awesome-package
+```
---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist'
+    }),
+    resolve(),
+    commonjs()
+  ],
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})]
+};
