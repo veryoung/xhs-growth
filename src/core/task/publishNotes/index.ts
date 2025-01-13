4fa2ef315c70424f05081ae9e5f29c62d7a9103c
lijiaxin7 <lijiaxin7@xiaohongshu.net>
2025-01-13 13:21:07 +0800

    readme改动

--- src/core/task/publishNotes/index.ts
+++ src/core/task/publishNotes/index.ts
@@ -9,21 +9,17 @@ const filterPageIds = (ids: string) => {
 export class PublishNotesTask  {
   //    发布笔记
   publish(pageId: string){
-    console.log('pageId', pageId)
     const publishNotePage = genCapaPostDeeplink({
       attach: { topics: filterPageIds(pageId as string) },
       config: {
         is_post_jump: 0,
-        // callback: `xhsdiscover://webview/${window.location.href.split('//')[1]}&from=post`,
       },
     })
-    console.log('publishNotePage', publishNotePage);
     go(publishNotePage, {
       type: 'deeplink',
       fail: (res: any)=>{
         console.log('error', res)
       }
     })
-    return 1
   }
 }
