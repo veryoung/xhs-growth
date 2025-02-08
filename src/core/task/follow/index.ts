72011bd6de5ed20679f54057ba1d7eca4856eac5
mofeng <wanyang@xiaohongshu.com>
2025-02-08 11:43:07 +0800

    feat: 设置关注任务跳转

--- src/core/task/follow/index.ts
+++ src/core/task/follow/index.ts
@@ -1,19 +1,47 @@
-import GrowthCore from "../../../index";
+import { TaskStatus } from "../../../types/task.d";
+import GrowthCore, { go } from "../../../index";
 
 export class FollowTask {
   /** 关注 */
-  async takeFollow(taskMetaId: string) {
-    const res = await GrowthCore.task.claimTask(taskMetaId)
-    if (res.code === 0) {
-      console.log("🚀 ~ FollowTask ~ takeFollow ~ res:", res)
-      return {
-        code: res.code,
-        msg: res.msg,
+  async takeFollow(taskMetaId: string, isAutoFollow: boolean = true) {
+    try {
+      const res = await GrowthCore.task.claimTask(taskMetaId)
+      if (res.code === 0) {
+        const { taskStatus, triggerCondition } = res.data
+        if(taskStatus === TaskStatus.UNFINISHED && isAutoFollow) { 
+          const ids = JSON.parse(triggerCondition)
+          go(`xhsdiscover://user/${ids[0]}`, {
+            type: 'deeplink',
+            success: (res) => {
+              return {
+                code: res.code,
+                msg: res.msg,
+              }
+            },
+            fail: (err) => {
+              return {
+                code: -300,
+                msg: err.message || '跳转失败',
+              }
+            }
+          });
+          return
+        }
+        console.log("🚀 ~ FollowTask ~ takeFollow ~ res:", res)
+        return {
+          code: res.code,
+          msg: res.msg,
+        }
       }
-    }
-    return {
-      code: res.code || -200,
-      msg: res.msg || '领取任务失败',
+      return {
+        code: res.code || -200,
+        msg: res.msg || '领取任务失败',
+      }
+    } catch (error: any) {
+      return {
+        code:  -200,
+        msg: error?.message || '领取任务失败',
+      }
     }
   }
 }
---
+++ src/types/task.d.ts
@@ -0,0 +1,11 @@
+/**
+ * 任务状态
+ */
+export enum TaskStatus {
+    /** 未完成 */
+    UNFINISHED = 'UNFINISHED',
+    /** 已完成 */
+    FINISHED = 'FINISHED',
+    /** 未领取 */
+    UNCLAIMED = 'UNCLAIMED',
+}
